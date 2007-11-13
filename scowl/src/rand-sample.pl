84212a60fa14201a761956c7de83a18f569cf47e
Kevin Atkinson <kevina@gnu.org>
2007-11-13 04:36:19 -0700

    Initial checkin of SCOWL version 6
    [svn rev 2]

---
+++ scowl/src/rand-sample.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+while (<STDIN>) {
+  chop;
+  push @words, $_;
+}
+@words = sort @words;
+@num = (0 .. $#words);
+
+@use = ();
+
+for ($i = 0; $i < $ARGV[0]; ++$i) {
+  $r=int rand @num;
+  push @use, splice @num, $r, 1;
+}
+
+@use = sort {$a <=> $b} @use;
+
+foreach $i (@use) {
+  print "$words[$i] ";
+}
+print "\n";

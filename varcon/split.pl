845ff8ab24f8c7500d1c36f63d0f534a46323246
Kevin Atkinson <kevina@gnu.org>
2007-11-22 23:17:29 -0700

    More fixes.
    Marked entries in which the American spelling is preferred.  From the README:
      The last column is used for miscellaneous notes.  With the following
      meaning (C-OED = Compact Oxford English Dictionary)
        ...
        VV: According to C-OED the American spelling is the correct one,
            however, I found some evidence that the British spelling is
            still used.
        V: According to C-OED the American spelling is the preferred form,
            however, the British spelling is also correct
        V?: Same as V, but it seamed inconsistent to me.  Explained below.
        v: According to C-OED the British spelling is the preferred form,
            however the American spelling is also correct.
    
    Updated split to take variant info into account.
    
    Hopefully now, SCOWL will have correct British English.
    
    [svn rev 39]

--- varcon/split.pl
+++ varcon/split.pl
@@ -5,13 +5,37 @@ open F, "abbc.tab";
 open A, ">american.lst.tmp";
 open B, ">british.lst.tmp";
 open Z, ">british_z.lst.tmp";
+open V, ">british_variant.lst.tmp";
 open C, ">canadian.lst.tmp";
 open c, ">common.lst.tmp";
+
+my %VARIANT_MAP = ('VV' => 3, 'V' => 2, 'V?' => 1, 'v' => 0);
+
+my $variant_level = 1;
+
+if (@ARGV >= 1) {
+    $variant_level = $VARIANT_MAP{$ARGV[0]};
+}
+
+if (!defined $variant_level || @ARGV > 1) {
+    die "Usage: $0 VV|V|V?|v\n";
+}
 
 while (<F>) {
     ($a,$b,$z,$c,$s) = /(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S*)\n/ 
 	or die "BadLine: $_";
-    if ($s eq "") {
+    if ($s =~ /^(VV|V|V\?|v)$/) {
+        die unless $b eq $z;
+        if ($variant_level{$s} >= $variant_level) {
+            $c = $a if $c eq $b;
+            $b = $a;
+            $z = $a;
+        } else {
+            print V "$a\n";
+        }
+        $s = '';
+    }
+    if ($s =~ /^!?$/) {
 	print A "$a\n";
 	print B "$b\n";
 	print Z "$z\n";
@@ -19,13 +43,15 @@ while (<F>) {
     } else {
         die unless $b eq $z;
 	print c "$a\n$b\n$c\n";
-	if ($s eq 'A') {
+	if ($s eq 'A' || $s eq 'a') {
 	    $al = "$a\n";
 	    $bl = "$a\n$b\n";
 	} elsif ($s eq 'B') {
 	    $al = "$a\n$b\n";
 	    $bl = "$b\n";
-	}
+        } else {
+            die "Unknown flag: $s\n";
+        }
 	print A $al;
 	print B $bl;
 	print Z $bl;

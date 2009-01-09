8308d5628208de9d59a33aabb68f1d61c36e8198
Kevin Atkinson <kevina@gnu.org>
2009-01-08 22:38:35 -0700

    Fix split so the output makes sence.
    Include British only variates in the variant_2 category.
    
    [svn rev 78]

--- varcon/split.pl
+++ varcon/split.pl
@@ -9,24 +9,18 @@ open V, ">british_variant.lst.tmp";
 open C, ">canadian.lst.tmp";
 open c, ">common.lst.tmp";
 
-my %VARIANT_MAP = ('VV' => 3, 'V' => 2, 'V?' => 1, 'v' => 0);
+#my %variant_level = ('VV' => 3, 'V' => 2, 'V?' => 1, 'v' => 1);
+my %variant_dir   = ('VV' => 'a', 'V' => 'a', 'V?' => 'a', 'v' => 'b');
 
-my $variant_level = 1;
-
-if (@ARGV >= 1) {
-    $variant_level = $VARIANT_MAP{$ARGV[0]};
-}
-
-if (!defined $variant_level || @ARGV > 1) {
-    die "Usage: $0 VV|V|V?|v\n";
-}
+die "Usage: $0\n" if (@ARGV > 0);
 
 while (<F>) {
     ($a,$b,$z,$c,$s) = /(\S+)\t(\S+)\t(\S+)\t(\S+)\t(\S*)\n/ 
 	or die "BadLine: $_";
     if ($s =~ /^(VV|V|V\?|v)$/) {
         die unless $b eq $z;
-        if ($variant_level{$s} >= $variant_level) {
+        if ($variant_dir{$s} eq 'a') {
+            print V "$b\n";
             $c = $a if $c eq $b;
             $b = $a;
             $z = $a;
@@ -44,6 +38,7 @@ while (<F>) {
         die "$b != $z" unless $b eq $z;
 	print c "$a\n$b\n$c\n";
 	if ($s eq 'A' || $s eq 'a') {
+            print $_ if $s eq 'a';
 	    $al = "$a\n";
 	    $bl = "$a\n$b\n";
 	} elsif ($s eq 'B') {

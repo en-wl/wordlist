fc827e0254f0c1830a9edc70780f88737f68ff4f
Kevin Atkinson <kevina@gnu.org>
2010-03-07 23:11:38 -0700

    Added util to sort varcon.txt.
    [svn rev 94]

---
+++ varcon/varcon-sort.pl
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+
+use varcon;
+
+my %data;
+
+my $infile;
+my $outfile;
+my $rename = 0;
+
+if (@ARGV == 1) {
+    $infile = $ARGV[0];
+    $outfile = "$ARGV[0].new";
+    $rename = 1;
+} elsif (@ARGV == 2) {
+    $infile = $ARGV[0];
+    $outfile = $ARGV[0];
+} else {
+    $infile = "/dev/stdin";
+    $outfile = "/dev/stdout";
+}
+
+open F, $infile or die;
+
+while (<F>) {
+    my $line = $_;
+    my %d = varcon::readline($line);
+    push @{$data{"$d{'A'}[0][0]"}}, $line;
+}
+
+open F, ">$outfile" or die;
+
+foreach my $key (sort keys %data) {
+    foreach (@{$data{$key}}) {
+        print F $_;
+    }
+}
+
+if ($rename) {
+    unlink $infile;
+    rename $outfile, $infile;
+}

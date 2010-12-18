771b3877843bdd7f345660e845dc9c53b1af7985
Kevin Atkinson <kevina@gnu.org>
2010-12-18 04:56:18 -0700

    Start varifing clusters against Oxford dictionaries (and sometimes M-W) and mark them as either varified or hand-checked based on the level of attention I gave an entry.  Made many corrections along the way.
    Also add (and correct!) some clusters discovered from Tysto UK vs US 
    spelling page (http://www.tysto.com/articles05/q1/20050324uk-us.shtml).
    
    [svn rev 105]

--- varcon/varcon.pm
+++ varcon/varcon.pm
@@ -2,14 +2,22 @@ package varcon;
 
 use strict;
 use warnings;
+use Carp;
+
+$SIG{__DIE__} = sub {die $_[0] unless defined $^S;
+                     confess $_[0];};
 
 use Exporter ();
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(readline flatten get_words filter %map);
+our @EXPORT_OK = qw(readline flatten get_words filter %map MAX_VARIANT_LEVEL);
 
 our %map = qw(A american B british Z british_z C canadian _ other);
-my %vmap = ('' => 0, 'v' => 1, 'V' => 2, '-' => 3,
-            '0' => 0, '1' => 1, '2' => 2, '3' => 3);
+my %vmap = ('' =>  -1, '.' => 0, 'v' => 1, 'V' => 2, '-' => 3, 'x' => 8,
+                       '0' => 0, '1' => 1, '2' => 2, '3' => 3, '8' => 8);
+my %rmap = ('' => '', 
+            -1 => '', 0 => '.', 1 => 'v', 2 => 'V', 3 => '-', 8 => 'x');
+
+our $MAX_VARIANT_LEVEL = 9;
 
 sub readline_no_expand($;$) {
     local $_ = shift;
@@ -19,11 +27,16 @@ sub readline_no_expand($;$) {
     my (@d) = split / *\/ */, $d;
     my %r;
     foreach (@d) {
-        my ($s, $w) = /^(.+?): (.+)$/ or die "Bad entry: $_";
+        my ($s, $w) = /^(.+?): (.+)$/ or croak "Bad entry: $_";
         my @s = split / /, $s;
         foreach (@s) {
-            my ($s, $v) = /^([ABZC_])([01234vV-]?)$/ or die "Bad category: $_";
-            push @{$r{$s}[$vmap{$v}]}, $w;
+            my ($s, $v) = /^([ABZC_*])([.01234vVx-]?)$/ or croak "Bad category: $_";
+            push @{$r{$s}[$vmap{$v}+1]}, $w;
+        }
+    }
+    foreach my $s (keys %r) {
+        if (defined $r{$s}[0] && @{$r{$s}[0]} > 1) {
+            unshift @{$r{$s}[1]}, splice(@{$r{$s}[0]}, 1);
         }
     }
     die if @n > 1;
@@ -51,15 +64,54 @@ sub flatten(%) {
     foreach my $k (keys %p) {
         next unless defined $p{$k};
         die "?$k" unless defined $p{$k}[0];
-        my @d = @{$p{$k}[0]};
-        $r{$k} = [shift @d];
-        $r{"${k}0"} = [@d] if @d;
-        foreach my $v (1..3) {
-            next unless defined $p{$k}[$v];
-            $r{"$k$v"} = $p{$k}[$v];
+        #my @d = @{$p{$k}[0]};
+        #$r{$k} = [shift @d];
+        #$r{"${k}0"} = [@d] if @d;
+        foreach my $v (-1..$MAX_VARIANT_LEVEL) {
+            next unless defined $p{$k}[$v+1];
+            my $vs = $v == -1 ? '' : $v;
+            $r{"$k$vs"} = $p{$k}[$v+1];
         }
     }
     return %r;
+}
+
+sub reverse($) {
+    my ($flattened) = @_;
+    my %r;
+    foreach my $tag (sort keys %$flattened) {
+        my $words = $flattened->{$tag};
+        foreach my $word (@$words) {
+            push @{$r{$word}}, $tag;
+        }
+    }
+    return bless \%r, 'varcon::revered';
+}
+
+sub format($) {
+    my ($flattened) = @_;
+    my $line = '';
+    my $reversed = varcon::reverse($flattened);
+    my %grouped;
+    foreach (sort keys %$flattened) {
+        /^(.)(.?)$/ or croak "Bad: $_";
+        $grouped{$1} .= "$2:@{$flattened->{$_}} ";
+    }
+    my %kill;
+    $kill{Z} = 1 if defined $grouped{Z} && defined $grouped{B} && $grouped{Z} eq $grouped{B};
+    $kill{C} = 1 if defined $grouped{C} && defined $grouped{Z} && $grouped{C} eq $grouped{Z};
+    foreach my $key (sort keys %$flattened) {
+        my $words = $flattened->{$key};
+        foreach my $word (@$words) {
+            next unless exists $reversed->{$word};
+            my @sps = grep {/^(.)(.?)$/; !$kill{$1}} @{$reversed->{$word}};
+            $line .= ' / ' unless $line eq '';
+            $line .= join(' ', map {/^(.)(.?)$/; "$1$rmap{$2}"} @sps);
+            $line .= ": $word";
+            delete $reversed->{$word};
+        }
+    }
+    return $line;
 }
 
 sub get_words_set(\%$) {
@@ -86,7 +138,7 @@ sub get_words_set(\%$) {
         my ($d) = split / *\| */;
         my (@d) = split / *\/ */, $d;
         foreach (@d) {
-            my ($s, $w) = /^(.+?): (.+)$/ or die "Bad entry: $_";
+            my ($s, $w) = /^(.+?): (.+)$/ or croak "Bad entry: $_";
             $res->{$w} = $i unless exists $res->{$_};
             $i++;
         }
@@ -99,8 +151,9 @@ sub get_words($) {
     return sort {$res{$a} <=> $res{$b}} keys %res;
 }
 
-sub filter(\$) {
-    ${$_[0]} =~ s/^([^#\n]*).*/$1/;
+sub filter(\$;$) {
+    ${$_[0]} =~ s/^([^#\n]*)(.*)/$1/;
+    ${$_[1]} = $2 if defined $_[1] && $2 ne '';
     ${$_[0]} =~ s/\s+$//;
     return 1 if ${$_[0]} eq '';
     return 0;

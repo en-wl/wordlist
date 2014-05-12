a6cb416a3299ce0745fa006f43e7bca4586e8421
Kevin Atkinson <kevina@gnu.org>
2014-05-12 13:10:38 +0200

    In make-hunspell-dict, also generate en_GB hunspell dictionaries.
    In addition update the README to make it more generic and make the
    it's generation a script that gets the version, date, and level from
    the environment.  If these are not provide in the environment sensible
    defaults are used. The version defaults the SVN version if the working
    copy was checked out from SVN.
    
    [svn rev 183]

--- scowl/speller/README_en.txt.sh
+++ scowl/speller/README_en.txt.sh
@@ -1,10 +1,22 @@
-Version 7.1-0
-2011-01-06
+#VERSION="Version 7.1-0"
 
-README file for en_US and en_CA Hunspell dictionaries
+if [ -e .svn ];
+then
+  : ${VERSION:="SVN Verson `svnversion`"}
+else
+  : ${VERSION:="Version 7.1 (ish)"}
+fi
+: ${DATE:=`date +'%F'`}
+: ${LEVEL:=60}
+
+cat <<EOF
+$VERSION
+$DATE
+
+README file for English Hunspell dictionaries derived from SCOWL.
 
 These dictionaries are created using the speller/make-hunspell-dict
-script in SCOWL, version 7.1 released on January 6, 2011.
+script in SCOWL.
 
 The NOSUGGEST flag was added to certain taboo words.  While I made an
 honest attempt to flag the strongest taboo words with the NOSUGGEST
@@ -20,10 +32,15 @@ wordlist-devel at sourceforge net mailing list.
 
 COPYRIGHT, SOURCES, and CREDITS:
 
-The en_US and en_CA dictionaries come directly from SCOWL (up to level
-60) and is thus under the same copyright of SCOWL.  The affix file is
+The English dictionaries come directly from SCOWL (up to level
+$LEVEL) and is thus under the same copyright of SCOWL.  The affix file is
 a heavily modified version of the original english.aff file which was
 released as part of Geoff Kuenning's Ispell and as such is covered by
 his BSD license.  Part of SCOWL is also based on Ispell thus the
 Ispell copyright is included with the SCOWL copyright.
 
+EOF
+
+cat ../Copyright
+echo
+echo "Build Date: `date`"

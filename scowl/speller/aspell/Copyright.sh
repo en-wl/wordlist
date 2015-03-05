7f8f6a1f0dc375b4f9ac45c62a36e13281a97096
J William Piggott <elseifthen@gmx.com>
2015-03-04 19:19:41 -0500

    aspell/Copyright.sh: use correct dict size
    scowl/speller/aspell/Copyright.sh
    scowl/speller/make-aspell-dict
    
    Signed-off-by: J William Piggott <elseifthen@gmx.com>

--- scowl/speller/aspell/Copyright.sh
+++ scowl/speller/aspell/Copyright.sh
@@ -1,8 +1,10 @@
 cat <<EOF
-This English word list is comes directly from SCOWL $SCOWL_VERSION (up to level 60,
-using the speller/make-aspell-dict script, http://wordlist.sourceforge.net/) 
+This English word list is comes directly from SCOWL $SCOWL_VERSION
+(up to level $SIZE, using the speller/make-aspell-dict script)
 and is thus under the same copyright of SCOWL.  The affix file (only
 included in the aspell6 package) is based on the Ispell one which is
 under the same copyright of Ispell.  Part of SCOWL is also based on
 Ispell thus the Ispell copyright is included with the SCOWL copyright.
+http://wordlist.sourceforge.net
+
 EOF

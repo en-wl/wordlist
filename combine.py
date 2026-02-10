3bc84025fdf75c3b693c03bf3ba78bc4f4a04fb9
Kevin Atkinson <kevina@gnu.org>
2026-02-09 19:25:04 -0500

    combine.py: reorder application of merge/adjust data so adjust comes first

--- combine.py
+++ combine.py
@@ -34,9 +34,9 @@ if len(sys.argv) < 2:
     usage()
 
 if sys.argv[1] == 'sort':
-    for fn in mergeFiles:
-        sortFileInPlace(files=[fn])
     for fn in adjustFiles:
+        sortFileInPlace(files=[fn])
+    for fn in mergeFiles:
         sortFileInPlace(files=[fn])
     exit(0)
 
@@ -122,16 +122,16 @@ with open('data/compounds-auto') as f:
                   groupComment = 'Compound variant levels are a best guess based on freq and other related info.')
 finish()
 
-for fn in mergeFiles:
-    start(fn)
-    with open(fn) as f:
-        mergeEntries(conn, f)
-    finish()
-
 for fn in adjustFiles:
     start(fn)
     with open(fn) as f:
         adjustEntries(conn, f, simplifyScowlInfo=False)
+    finish()
+
+for fn in mergeFiles:
+    start(fn)
+    with open(fn) as f:
+        mergeEntries(conn, f)
     finish()
 
 if not rawMode:

a285c6320e401eae6af0431f6de4f82f8302c4e6
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-02-14 11:03:26 +0100

    Apply ruff/flake8-simplify rule SIM210
    Remove unnecessary `True if ... else False`
    Use `bool(...)` instead of `True if ... else False`

--- libscowl/__main__.py
+++ libscowl/__main__.py
@@ -100,7 +100,7 @@ class Lst(argparse.Action):
     def __call__(self, parser, namespace, values, option_string):
         lst = [v.strip() for v in values.split(',')]
         cls = Exclude if option_string.startswith('--wo-') else Include
-        noDefault = True if 'no-default' in lst else False
+        noDefault = 'no-default' in lst
         lst = cls(*(v for v in lst if v != 'no-default'), noDefault = noDefault)
         setattr(namespace, self.dest, lst)
 
@@ -210,7 +210,7 @@ def addQueryArguments(p, usePositional):
     addArg('--regions', type=lst, metavar='<list>',
            help=f"any of: {', '.join(REGIONS[1:])}")
     variantSymbolsStr = ','.join(symbol if symbol.isalnum() else f"'{symbol}'" for symbol in variantFromSymbol.keys())
-    grp = p.add_mutually_exclusive_group(required=True if usePositional else False)
+    grp = p.add_mutually_exclusive_group(required=bool(usePositional))
     addArg('--variant-level', metavar='<char>', choices=[*variantFromSymbol.keys(),*map(str, range(10))], dest='variantLevel',
            help=f"one of: {variantSymbolsStr},0-9", optional = True, grp = grp)
     addArg('--variant-levels', action=VariantLevels, dest='variantLevels', metavar='<list>', grp = grp)
